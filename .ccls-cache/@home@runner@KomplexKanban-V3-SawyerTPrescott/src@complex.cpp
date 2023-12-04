8e393a4a32fafea99cea87c7dab6954f4d10e18f
Preficks <90790194+Preficks@users.noreply.github.com>
2023-12-04 15:32:21 +0000

    Test

---
+++ .ccls-cache/@home@runner@KomplexKanban-V3-SawyerTPrescott/src@complex.cpp
@@ -0,0 +1,7 @@
+class complex{
+
+    double real;
+    double img;    
+    complex(){img=0;real=0;}
+    complex(double r, double i){real=r;img=i}
+};
---
+++ src/Complex.cpp
@@ -0,0 +1,7 @@
+class Complex{
+
+    double real;
+    double img;    
+    Complex(){img=0;real=0;}
+    Complex(double r, double i){real=r;img=i;}
+};
